0980538ca296272c7d1b0ca3da216b527c00d040
fai-k <thee.kochakarn@gmail.com>
2024-02-06 14:18:36 +0100

    Edit T89 ref

--- pipeline/submitSTARGenomeGenerate.sh
+++ pipeline/submitSTARGenomeGenerate.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#SBATCH -p core
+#SBATCH -t 12:00:00
+#SBATCH -n 4
+#SBATCH -A u2023008
+
 set -eu
 export SINGULARITY_BINDPATH="/mnt:/mnt"
 
 proj=u2023008
-genomedir=/mnt/ada/projects/aspseq/T89-assembly/data/
 outdir=/mnt/ada/projects/aspseq/mschmid/poplar-CRISPR-WGS/RNA-seq/preprocessed/T89_STAR/
 star=/mnt/picea/storage/singularity/kogia/star_2.7.10a.sif
 
 mkdir -p $outdir/index
-#zcat $genomedir/T89.hap1.fa.gz $genomedir/T89.hap2.fa.gz > $outdir/genome.fa
+zcat /mnt/picea/storage/reference/Populus-tremula_X_Populus-tremuloides/v2.0/fasta/primary-plus-alternative-haplotypes.fasta.gz > $outdir/genome.fa
 sbatch -A $proj ./runSTARGenomeGenerate.sh -n $star $outdir/index $outdir/genome.fa
