4a1713aee2420996b56444ace4490cf900d2d042
krbe9202 <kristina.benevides@umu.se>
2024-03-19 08:53:32 +0100

    Added scripts for merging BAM files

---
+++ pipeline/submitMergeBams.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+set -eu
+
+# define variables
+proj=u2023008
+email=kristina.benevides@umu.se
+in=/mnt/ada/projects/aspseq/mschmid/poplar-CRISPR-WGS/RNA-seq/preprocessed/STAR
+out=/mnt/ada/projects/aspseq/mschmid/poplar-CRISPR-WGS/RNA-seq/preprocessed/STAR/merged
+reference=$(realpath ../reference)
+samtools_sif=$(realpath ../singularity/kogia/samtools_1.16.sif)
+
+if [ ! -d $out ]; then
+  mkdir -p $out
+fi
+
+# env
+export SINGULARITY_BINDPATH="/mnt:/mnt"
+
+prefixes=("L03" "L26" "T89")
+
+for prefix in "${prefixes[@]}"; do
+    input_files=("$in/${prefix}_*".bam)  # Collect files with the same prefix
+    sbatch -A $proj --mail-user $email -J "${prefix}_BAM_merge" \
+         runMergeBams.sh $samtools_sif $input_files $out
+done
+
+# run
+#for BAM in $(find $in -name "*.bam$"); do
+
+#sbatch -A $proj --mail-user $email -J $(basename ${BAM/.sorted.bam/}) \
+#	runMergeBams.sh $samtools_sif \
+#	$BAM $out
+#done
